1e5df2a7012f03ab21bfc33bb74581a53098a2e4
Ivan Milinkovic <ivan.milinkovic@htecgroup.com>
2025-04-09 16:41:01 +0200

    Randomize data

--- books-express-ts-api/src/data.ts
+++ books-express-ts-api/src/data.ts
@@ -45,11 +45,30 @@ const authors = authorIndices.map((i): Author => {
 
 const bookIndices = Array.from({ length: 50 }, (val, index) => index);
 
+function randomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+// At least one
+function randomSlice1<T>(array: T[]): T[] {
+  const start = randomInt(0, array.length - 1);
+  const end = randomInt(start + 1, array.length);
+  return array.slice(start, end);
+}
+
 const books = bookIndices.map((i): Book => {
   // find radnom authors for a book
-  const start = Math.random() * authors.length;
-  const len = Math.random() * 5;
-  const someAuthors = authors.slice(start, start + len);
+  // const start = Math.random() * authors.length;
+  // const len = Math.random() * 5;
+  // const someAuthors = authors.slice(start, start + len);
+  const someAuthors = randomSlice1(authors);
+
+  // pick some genres
+  const forChildren = Math.random() >= 0.5;
+  let genres: Genre[] = forChildren
+    ? randomSlice1(childrenGenres)
+    : randomSlice1(adultGenres);
+
   return {
     id: i,
     title: `Book ${i}`,
@@ -58,9 +77,9 @@ const books = bookIndices.map((i): Book => {
     quantity: 30,
     publishDate: new Date(2020, i % 12, i % 28),
     pageCount: 200,
-    genres: [nonFictionGenres[0], nonFictionGenres[1]],
+    genres: genres,
     authors: someAuthors,
-    forChildren: true,
+    forChildren: forChildren,
     image: null,
   };
 });
