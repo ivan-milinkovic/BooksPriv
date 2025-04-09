0257a23420bfadf631bde041e5931961bb33d497
Ivan Milinkovic <ivan.milinkovic@htecgroup.com>
2025-04-09 10:32:00 +0200

    Client: basic styles

--- books-client/src/App.tsx
+++ books-client/src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <>Root</>;
+  return (
+    <div className="h-full">
+      Root
+      <button className="primary-button">Button</button>
+      <button className="secondary-button">Button</button>
+      <a href="#" className="link">
+        Link
+      </a>
+    </div>
+  );
 }
 
 export default App;
