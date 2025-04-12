cfd5f853d412c4255c1bf716d958ee2092ca12d5
Ivan Milinkovic <ivanmcode@gmail.com>
2025-04-12 14:28:23 +0200

    Update run script

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #! /bin/zsh
 
-server_script="cd '$PWD/books-server' && pwd && npm run dev"
+server_script="cd '$PWD/books-server' && pwd && npm ci && npm run dev"
 server_term_command="tell app \"Terminal\" to do script \"$server_script\""
 osascript -e $server_term_command
 
-client_script="cd '$PWD/books-client' && pwd && npm run dev"
+client_script="cd '$PWD/books-client' && pwd && npm ci && npm run dev"
 client_term_command="tell app \"Terminal\" to do script \"$client_script\""
 osascript -e $client_term_command
 
