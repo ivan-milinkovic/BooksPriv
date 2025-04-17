bf1dcc404db807b437d9e67d39b336688ac86618
Ivan Milinkovic <ivanmcode@gmail.com>
2025-04-17 11:59:55 +0200

    Remove asp.net directory

--- BooksNetApi/Model/Author.cs
+++
@@ -1,6 +0,0 @@
-public class Author
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public List<Book> Books { get; set; } = [];
-}
--- BooksNetApi/Model/Genre.cs
+++
@@ -1,5 +0,0 @@
-public class Genre
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-}
